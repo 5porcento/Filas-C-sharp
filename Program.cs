3a9f7299cbd355be07eba743ef705ce2c25cd9d4
Kauan Pacheco <kauanpacheco.dev@gmail.com>
2024-01-27 11:55:08 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+Queue<int> fila = new Queue<int>();
+
+fila.Enqueue(1);
+fila.Enqueue(2);
+fila.Enqueue(3);
+fila.Enqueue(4);
+fila.Enqueue(5);
+fila.Enqueue(6);
+
+foreach (int itens in fila)
+{
+    Console.WriteLine(itens);
+}
+
+Console.WriteLine($"Removendo o item: {fila.Dequeue()}");
+fila.Enqueue(7);
+
+foreach (int itens in fila)
+{
+    Console.WriteLine(itens);
+}
